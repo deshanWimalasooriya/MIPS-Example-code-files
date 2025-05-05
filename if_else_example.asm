b42ae9834408381b2e8940bd17c89d0f95b2589c
deshanWimalasooriya <deshanwimalasooriya000@gmail.com>
2025-05-05 16:35:00 +0530

    add if_else_example.asm file

--- if_else_example.asm
+++ if_else_example.asm
@@ -1,28 +1,40 @@
+# MIPS Program to perform:
 # if (i == j)
 #     f = g + h;
 # else
 #     f = g - h;
+
 .data
-newline: .asciiz "\n"  # Define a newline character
+f:      .word 0         # f = 0 (to store result)
+newline: .asciiz "\n"   # newline for clean output
+
 .text
 .globl main
 
 main:
-    # Load values
-    li $t0, 5        # $t0 = i
-    li $t1, 5        # $t1 = j
-    li $t2, 10       # $t2 = g
-    li $t3, 3        # $t3 = h
+    # Load variables into registers
+    li $t0, 5           # $t0 = i
+    li $t1, 6           # $t1 = j
+    li $t2, 10          # $t2 = g
+    li $t3, 3           # $t3 = h
 
-    # add g+h
-    add $t4, $t2, $t3  # $t4 = g + h
+    # Compare i and j
+    beq $t0, $t1, equal_case
 
-    #sub g-h
-    sub $t5, $t2, $t3  # $t5 = g - h
+    # else part: f = g - h
+    sub $t4, $t2, $t3
+    sw $t4, f
+    j end_if
 
-    #print $t4
-    li $v0, 1        # Syscall to print integer
-    move $a0, $t4    # Move result to $a0
+equal_case:
+    # if part: f = g + h
+    add $t4, $t2, $t3
+    sw $t4, f
+
+end_if:
+    # Print result f
+    move $a0, $t4       # move result to $a0
+    li $v0, 1           # print_int syscall
     syscall
 
     # Print newline
@@ -30,12 +42,6 @@ main:
     la $a0, newline
     syscall
 
-    #print $t5
-    li $v0, 1        # Syscall to print integer
-    move $a0, $t5    # Move result to $a0
-    syscall
-
-    #exit
+    # Exit
     li $v0, 10
     syscall
-# The above code is a MIPS assembly program that demonstrates the use of conditional statements
